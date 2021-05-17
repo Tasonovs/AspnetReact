49349bca2f1291cd8f175dc11c602a837b1d2ee0
Швырёв Станислав <ctac00025@gmail.com>
2021-05-17 13:57:39 +0300

    Edit Home page

--- AspnetReact/ClientApp/src/pages/Home.js
+++ AspnetReact/ClientApp/src/pages/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import * as Api from "api/Requests"
+import { Container } from 'react-bootstrap';
+import { LoadingAndErrors } from 'components/common';
+import CampaignCard from 'components/campaigns/CampaignCard';
+export default function Home() {
+  const [data, isLoading, error] = Api.useGetRequest(Api.Routes.Campaign + "page/1");
 
-export default class Home extends Component {
-  static displayName = Home.name;
-
-  render() {
-    return (
-      <h1>Hello</h1>
-    );
-  }
+  return (
+    <Container>
+      <h1>Recent campaigns</h1>
+      <hr />
+      <LoadingAndErrors data={data.campaigns} isLoading={isLoading} error={error} children={(
+        <Container className="d-inline-flex flex-wrap justify-content-around">
+          {data.campaigns?.map(item => (<CampaignCard key={item.id} campaign={item} />))}
+        </Container>
+      )} />
+    </Container>
+  );
 }
